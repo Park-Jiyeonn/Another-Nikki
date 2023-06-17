8bc50dc28e7a6846faccb69a4dd0c859a027dd5b
Jiyeon <jiyeonnn2@icloud.com>
2023-06-17 14:21:18 +0800

    use docker run to create a container

--- Another-Nikki-Server/code/c++.cpp
+++ Another-Nikki-Server/code/c++.cpp
@@ -1,9 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+const int N = 1e6 + 5, mod = 1e9 + 7;
+int a[N];
 void solve()
 {
     cout << "Hello Docker, you can make a online judge !!!\n";
+    int n = 1e6;
+    a[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        a[i] = a[i - 1] * i % mod;
+    }
 }
 signed main()
 {
