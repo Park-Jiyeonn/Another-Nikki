c363ba453ae031e7c4f6a9016437c767dfe739bf
Jiyeon <jiyeonnn2@icloud.com>
2023-06-18 22:35:02 +0800

    fix: concurrency bug

--- Another-Nikki-Server/router/run_code.go
+++ Another-Nikki-Server/router/run_code.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"Another-Nikki/util"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,8 +18,13 @@ type Code struct {
 	Lang string `json:"lang"`
 }
 
-func writeCodeInFile(code *Code) error {
-	var filename = "./code/c++.cpp"
+func writeCodeInFile(code *Code, ID int64) error {
+	dirPath := fmt.Sprintf("./code/tmp-%d", ID)
+	err := os.MkdirAll(dirPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	filename := dirPath + "/c++.cpp"
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666) //打开文件
 	if err != nil {
 		return err
@@ -25,6 +32,11 @@ func writeCodeInFile(code *Code) error {
 	defer f.Close()
 	_, err = io.WriteString(f, code.Code) //写入文件(字符串)
 	return err
+}
+
+func deleteFile(ID int64) {
+	dirPath := fmt.Sprintf("./code/tmp-%d", ID)
+	_ = os.RemoveAll(dirPath)
 }
 
 func runCommand(s string) error {
@@ -48,23 +60,31 @@ func RunCode(c *gin.Context) {
 		return
 	}
 
-	err = writeCodeInFile(&code)
+	ID := time.Now().UnixNano()
+	err = writeCodeInFile(&code, ID)
+	defer deleteFile(ID)
 	if util.HandleError(c, err, "写入c++.cpp失败") {
 		return
 	}
 
-	err = runCommand("docker run --rm -m 256m --name cpp_compile -v $(pwd)/code:/dox cpp_env:1 sh -c 'g++ 'c++.cpp' -o 'cpp' -O2 -std=c++11 2> compile.log'")
+
+	cmd1 := fmt.Sprintf("docker run --rm -m 256m --name cpp_compile-%d -v $(pwd)/code/tmp-%d:/dox cpp_env:1 sh -c 'g++ 'c++.cpp' -o 'cpp' -O2 -std=c++11 2> compile.log'", ID,ID)
+	cmd2 := fmt.Sprintf("docker run --rm -m 256m --name cpp_run-%d -v $(pwd)/code/tmp-%d:/dox cpp_env:1 sh -c 'ls ./.. && ./../calc/calc ./cpp > a.out'", ID,ID)
+	// fmt.Println(cmd1)
+	// fmt.Println(cmd2)
+
+	err = runCommand(cmd1)
 	if err != nil {
 		compile_log, _ := os.ReadFile("./code/compile.log")
 		util.HandleError(c, err, string(compile_log))
 		return
 	}
-	err = runCommand("docker run --rm -m 256m --name cpp_run -v $(pwd)/code:/dox cpp_env:1 sh -c './calc ./cpp > a.out'")
+	err = runCommand(cmd2)
 	if util.HandleError(c, err, "run failed") {
 		return
 	}
 
-	ret, err:= os.ReadFile("./code/a.out")
+	ret, err:= os.ReadFile(fmt.Sprintf("./code/tmp-%d/a.out", ID))
 	if util.HandleError(c, err, "read a.out failed") {
 		return
 	}
