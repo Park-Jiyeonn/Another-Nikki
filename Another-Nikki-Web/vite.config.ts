1061a9cbea87fca003ca8dca988e923a5d1b7f09
Jiyeon <jiyeonnn2@icloud.com>
2023-06-19 18:15:52 +0800

    fix: a bug

--- Another-Nikki-Web/vite.config.ts
+++ Another-Nikki-Web/vite.config.ts
@@ -5,13 +5,6 @@ import { resolve } from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
-  server: {
-		open: true,
-		port: 9999,
-		host: '0.0.0.0',
-	},
-
   // 设置路径别名
   resolve: {
     alias:{
