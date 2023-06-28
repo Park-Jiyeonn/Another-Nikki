c4fbb713a062a04cd1bc078f8fa6554123835130
Jiyeon <jiyeonnn2@icloud.com>
2023-06-28 16:30:36 +0800

    tset😿

--- Another-Nikki-Web/vite.config.ts
+++ Another-Nikki-Web/vite.config.ts
@@ -1,14 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path"
+import Components from "unplugin-vue-components/vite";
+import { VantResolver } from "unplugin-vue-components/resolvers";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  // 设置路径别名
-  resolve: {
-    alias:{
-      '@': resolve(__dirname, 'src'),
-    }
-}
+  plugins: [
+		vue(),
+		Components({
+			resolvers: [VantResolver()],
+		}),
+		createSvgIconsPlugin({
+			// 配置svg文件路径
+			iconDirs: [resolve(process.cwd(), "src/assets/svg")],
+			// 指定symbolId格式，规则
+			symbolId: "icon-[name]",
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "@/assets/css/base/index.scss" as *;',
+			},
+		},
+	},
+	base: '/',
 })
